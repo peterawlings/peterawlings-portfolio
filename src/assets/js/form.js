2396607c9cce2750643902320f1ee91a97e90db6
pete rawlings <rawlings.pete@gmail.com>
2017-01-17 20:39:59 +1100

    form thankyou,refinements

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -2,8 +2,10 @@ portfolio.form = (function () {
   'use strict';
 
   // variables
-  var $form      = $('#form');
-  var $formInput = $form.find('[data-js="formInput"]');
+  var $formContainer    = $('.c-form-container');
+  var $form             = $formContainer.find('form');
+  var $formThanks       = $formContainer.find('.c-form-thanks');
+  var $formInput        = $formContainer.find('[data-js="formInput"]');
 
   var initialize = function() {
     $formInput.on('focus blur', toggleFocus);
@@ -16,44 +18,30 @@ portfolio.form = (function () {
         $this.prev().removeClass('is-focused');
       }
     }
-
-  $form
-  // field element is invalid
-  .on("invalid.zf.abide", function(ev,elem) {
-    console.log("Field id "+ev.target.id+" is invalid");
-  })
-  // field element is valid
-  .on("valid.zf.abide", function(ev,elem) {
-    console.log("Field name "+elem.attr('name')+" is valid");
-  })
-  // form validation failed
-  .on("forminvalid.zf.abide", function(ev,frm) {
-    console.log("Form id "+ev.target.id+" is invalid");
-  })
-  // form validation passed, form will submit if submit event not returned false
-  .on("formvalid.zf.abide", function(ev,frm) {
-    console.log("Form id "+frm.attr('id')+" is valid");
-    $.ajax({
-      type: 'POST',
-      data: $(form).serialize(), // $(form) is ok here
-      url: 'http://stage.pete-rawlings.com/cgi-bin/mail.pl',
-      success: function(){
-        ev.preventDefault();
-        console.log('yes');
-        $form.fadeOut(1000);
-      },
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        ev.preventDefault();
-        console.log('no');
-      }
+    $('.test').click(function(){
+      $form.fadeOut(300).delay(2600).fadeIn(300);
+      $formThanks.delay(310).fadeIn(300).delay(2000).fadeOut(300);
     });
-  })
-  // to prevent form from submitting upon successful validation
-  .on("submit", function(ev) {
-    ev.preventDefault();
-    console.log("Submit for form id "+ev.target.id+" intercepted");
-  });
 
+    // TODO - fix this form shit
+    $form.on("formvalid.zf.abide", function(ev,frm) {
+      console.log("Form id "+frm.attr('id')+" is valid");
+      $.ajax({
+        type: 'POST',
+        data: $(form).serialize(), // $(form) is ok here
+        url: 'http://stage.pete-rawlings.com/cgi-bin/mail.pl',
+        success: function(){
+          ev.preventDefault();
+          console.log('yes');
+          $form.fadeOut(1000);
+          $formThanks.fadeIn(1000);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+          ev.preventDefault();
+          console.log('no');
+        }
+      });
+    })
   };
   return {
     init: initialize
